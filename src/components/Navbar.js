8064e27715a4b824a287b337ee287526558f233c
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2022-01-13 21:26:27 +0100

    added button + css magic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 
-import './Navbar.css'
+import './Navbar.css';
 
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -42,6 +54,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
